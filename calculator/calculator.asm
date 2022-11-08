bebe226f139f656f060ed16da4af23e3d6b66eca
Mistryu <michchrz42@gmail.com>
2022-11-08 09:58:03 +0100

    Failed attempt to do a calculator

---
+++ calculator/calculator.asm
@@ -0,0 +1,35 @@
+;nasm -f elf32 calculator.asm -o calculator.o && ld -m elf_i386 calculator.o -o calculator && ./calculator; echo $?
+
+
+;TODO: do a calculator that does additon subtraction multiplication and divison 3 inputs
+section .data
+    msg db "Calculator:", 10
+    len equ $ - msg
+    first_input db 100
+
+
+
+section .text
+    global _start
+
+_start:
+
+    mov edx, len        ; Dsplay "Calculator:""
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 0x80
+
+    mov ebx, 2          ; getting input from user
+    mov eax, 3
+    mov ecx, first_input
+    mov edx, 100
+    int 0x80
+
+    mov ebx, 1      ; print the 1st input
+    mov eax, 4
+    int 0x80
+
+    mov ebx, 0
+    mov eax, 1
+    int 0x80
